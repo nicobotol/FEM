ed9c4d007d5c149b79fb6194b980a53c1b568cd2
nicobotol <niccolo.andreetta@gmail.com>
2022-09-03 13:26:25 +0200

    Work on exercise 1.5 and DRY fea_ex1_2.m

---
+++ week1/competition/comp_3.m
@@ -0,0 +1,77 @@
+% Created with:       FlExtract v1.13
+% Element type:       truss
+% Number of nodes:    10
+% Number of elements: 17
+
+clear all;
+
+% Node coordinates: x, y
+X = [
+0.4	0.5
+0.5	0
+0.5	0.5
+0.5	0.62
+0.75	0.5
+0.75	0.62
+1.5	0
+1.5	0.5
+1.5	0.62
+1.6	0.5
+];
+% Element connectivity: node1_id, node2_id, material_id
+IX = [
+4	1	1
+3	1	1
+2	1	1
+3	2	1
+4	3	1
+5	3	1
+6	4	1
+4	5	1
+6	5	1
+5	9	1
+8	5	1
+9	6	1
+8	7	1
+10	7	1
+9	8	1
+10	8	1
+10	9	1
+];
+
+% IX = [
+% 4	1	1
+% 3	1	1
+% 2	1	1
+% 3	2	1
+% 4	3	1
+% 5	3	1
+% 6	4	1
+% 3	6	1
+% 6	5	1
+% 6	8	1
+% 8	5	1
+% 9	6	1
+% 8	7	1
+% 10	7	1
+% 9	8	1
+% 10	8	1
+% 10	9	1
+% ];
+% Element properties: Young's modulus, area
+mprop = [
+1	1
+2	2
+];
+% Nodal diplacements: node_id, degree of freedom (1 - x, 2 - y), displacement
+bound = [
+2	1	0
+2	2	0
+7	2	0
+];
+% Nodal loads: node_id, degree of freedom (1 - x, 2 - y), load
+loads = [
+  5, 2, -0.01
+];
+% Control parameters
+plotdof = 3;
