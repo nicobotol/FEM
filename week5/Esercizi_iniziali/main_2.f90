bd3e693d02bd9330bc3fc2d87603429dd0d9ef40
nicobotol <niccolo.andreetta@gmail.com>
2022-10-02 14:45:19 +0200

    Add continuum2 test file

--- week5/Esercizi_iniziali/main_2.f90
+++
@@ -1,8 +0,0 @@
-program IntegerPitfall
-  implicit none
-
-  real::x
-  x = 1.0
-
-  print*, ''
-end program
--- week5/fortranlecture/lecture2.f90
+++
@@ -1,6 +0,0 @@
-module Constants
-  
-  implicit none
-  real :: pi = 3.1415
-
-end module
